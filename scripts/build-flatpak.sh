49ae605bb474a89443d5ae9abf04160b020df82a
Dejan Noveski <dr.mote@gmail.com>
2026-03-27 12:05:23 +0100

    Fix the flatpak builder script

--- scripts/build-flatpak.sh
+++ scripts/build-flatpak.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 VERSION=$(git describe --tags --abbrev=0)
-flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak-builder --repo=flatpakrepo --force-clean flatpakbuild com.dekomote.vermouth.yml --no-fuse
+flatpak-builder --repo=flatpakrepo --force-clean flatpakbuild com.dekomote.vermouth.yml
 flatpak build-bundle flatpakrepo vermouth-${VERSION}.flatpak com.dekomote.vermouth
