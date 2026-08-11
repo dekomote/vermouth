bd9d0f8d76ac1a966bb912afa027cdd4e06f645d
Dejan Noveski <dr.mote@gmail.com>
2026-08-11 12:07:42 +0200

    Bump versions

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         default = pkgs.stdenv.mkDerivation {
           pname = "vermouth";
           dontWrapQtApps = true;
-          version = "2.0.1";
+          version = "2.0.2";
           src = ./.;
 
           nativeBuildInputs = with pkgs; [
